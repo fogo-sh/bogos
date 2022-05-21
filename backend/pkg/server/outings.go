48fcce7ad78d640f53c1497437dc21a7be3e86c3
Riley Flynn <riley@rileyflynn.me>
2022-05-21 12:53:21 -0230

    Use real data for outing list & outing user list

--- backend/pkg/server/outings.go
+++ backend/pkg/server/outings.go
@@ -25,75 +25,6 @@ type outingsService struct {
 	proto.UnimplementedOutingsServer
 }
 
-var testingOutings = map[int32]database.Outing{
-	1: {
-		ID:        1,
-		Title:     "First outing",
-		Date:      time.Date(2022, 4, 30, 0, 0, 0, 0, time.UTC),
-		CreatedAt: time.Date(2022, 4, 30, 0, 0, 0, 0, time.UTC),
-		UpdatedAt: sql.NullTime{},
-	},
-	2: {
-		ID:        2,
-		Title:     "Second outing",
-		Date:      time.Date(2022, 3, 30, 0, 0, 0, 0, time.UTC),
-		CreatedAt: time.Date(2022, 4, 30, 0, 0, 0, 0, time.UTC),
-		UpdatedAt: sql.NullTime{
-			Time:  time.Date(2022, 4, 30, 5, 0, 0, 0, time.UTC),
-			Valid: true,
-		},
-	},
-}
-
-var testOutingUsers = map[int32][]database.User{
-	1: {
-		{
-			ID:           1,
-			Username:     "jackharrhy",
-			PasswordHash: nil,
-			DisplayName: sql.NullString{
-				Valid:  true,
-				String: "Jack Harrhy",
-			},
-			AvatarUrl: sql.NullString{
-				Valid:  true,
-				String: "https://avatars.githubusercontent.com/u/6486417",
-			},
-			CreatedAt: time.Date(2022, 4, 30, 0, 0, 0, 0, time.UTC),
-		},
-	},
-	2: {
-		{
-			ID:           1,
-			Username:     "jackharrhy",
-			PasswordHash: nil,
-			DisplayName: sql.NullString{
-				Valid:  true,
-				String: "Jack Harrhy",
-			},
-			AvatarUrl: sql.NullString{
-				Valid:  true,
-				String: "https://avatars.githubusercontent.com/u/6486417",
-			},
-			CreatedAt: time.Date(2022, 4, 30, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			ID:           2,
-			Username:     "nint8835",
-			PasswordHash: nil,
-			DisplayName: sql.NullString{
-				Valid:  true,
-				String: "Riley Flynn",
-			},
-			AvatarUrl: sql.NullString{
-				Valid:  true,
-				String: "https://avatars.githubusercontent.com/u/1741548",
-			},
-			CreatedAt: time.Date(2022, 4, 30, 0, 0, 0, 0, time.UTC),
-		},
-	},
-}
-
 func (o *outingsService) CreateOuting(ctx context.Context, request *proto.CreateOutingRequest) (*proto.Outing, error) {
 	_, err := o.server.authorize(ctx)
 	if err != nil {
@@ -153,10 +84,16 @@ func (o *outingsService) UpdateOuting(ctx context.Context, request *proto.Update
 	return proto.DBOutingToProtoOuting(newOuting), nil
 }
 
-func (o *outingsService) ListOutings(_ context.Context, _ *emptypb.Empty) (*proto.ListOutingsReply, error) {
+func (o *outingsService) ListOutings(ctx context.Context, _ *emptypb.Empty) (*proto.ListOutingsReply, error) {
 	var resp []*proto.Outing
 
-	for _, outing := range testingOutings {
+	outings, err := o.server.db.ListOutings(ctx)
+	if err != nil {
+		log.Error().Str("operation", "ListOutings").Err(err).Msg("Error listing outings")
+		return nil, status.Error(codes.Internal, "")
+	}
+
+	for _, outing := range outings {
 		resp = append(
 			resp,
 			proto.DBOutingToProtoOuting(outing),
@@ -166,10 +103,16 @@ func (o *outingsService) ListOutings(_ context.Context, _ *emptypb.Empty) (*prot
 	return &proto.ListOutingsReply{Outings: resp}, nil
 }
 
-func (o *outingsService) ListOutingUsers(_ context.Context, request *proto.ListOutingUsersRequest) (*proto.ListOutingUsersReply, error) {
+func (o *outingsService) ListOutingUsers(ctx context.Context, request *proto.ListOutingUsersRequest) (*proto.ListOutingUsersReply, error) {
 	var resp []*proto.User
 
-	for _, user := range testOutingUsers[request.OutingId] {
+	outingUsers, err := o.server.db.ListUsersForOuting(ctx, request.OutingId)
+	if err != nil {
+		log.Error().Str("operation", "ListOutingUsers").Err(err).Msg("Error listing users for outing")
+		return nil, status.Error(codes.Internal, "")
+	}
+
+	for _, user := range outingUsers {
 		resp = append(
 			resp,
 			proto.DBUserToProtoUser(user),
